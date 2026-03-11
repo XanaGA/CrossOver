e1367a7a2a4780cbc5fa36ac3386f0a817949c8f
Xavier Anadon <xvanadon@gmail.com>
2026-03-11 08:49:33 +0100

    Config files to evaluate crossover

--- scripts/evaluation/eval_scene_retrieval.sh
+++ scripts/evaluation/eval_scene_retrieval.sh
@@ -2,6 +2,6 @@ export PYTHONWARNINGS="ignore"
 
 # Scene Retrieval Inference
 python run_evaluation.py --config-path "$(pwd)/configs/evaluation" --config-name eval_scene.yaml \
-task.InferenceSceneRetrieval.val=['ARKitScenes'] \
-task.InferenceSceneRetrieval.ckpt_path=/drive/dumps/multimodal-spaces/runs/UnifiedTrain_Scannet+Scan3R+MultiScan+ARKitScenes/2025-07-03-07:39:02.553100/ckpt/best.pth \
+task.InferenceSceneRetrieval.val=['Scannet'] \
+task.InferenceSceneRetrieval.ckpt_path=/home/xavi/CrossOver/checkpoints/model.safetensors \
 hydra.run.dir=. hydra.output_subdir=null 
